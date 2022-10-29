a112694a7a1de3bc5297d8d2575d01d4d03d059d
aqueralto <albert.queralto.lopez@gmail.com>
2022-10-29 17:54:48 +0200

    update user agents

--- src/webdriver_utils.py
+++ src/webdriver_utils.py
@@ -2,12 +2,14 @@
 
 class WebDriverOptions(object):
     """
-    Class that defines all the methods to setup the WebDriver
+    Class that stores all the variables to setup the WebDriver
     """
+    
+    # List of User Agents to loop from in order to avoid being identified as a scraper
     user_agents = [
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15"
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/98.0",
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.119 Safari/537.36",
             "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/84.0",
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
