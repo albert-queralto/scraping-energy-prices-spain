26ba0e83ff0c0021e35124a89393ce63aaabb06e
aqueralto <albert.queralto.lopez@gmail.com>
2022-10-29 17:54:05 +0200

    added date_picker function

--- src/navigation.py
+++ src/navigation.py
@@ -15,27 +15,45 @@ class BasePage(object):
         self.driver.maximize_window()
         
 class Navigation(BasePage):
+    """
+    Contains all the methods that enable navigation.
+    """
     def open_chrome_session(self):
+        """
+        Opens the Webdriver session on a Chrome browser.
+        
+        Parameters:
+        ----------
+        self.driver: object.
+                    The Webdriver object.
+        self.base_url: string.
+                        A string that contains the base URL.
+        """
         while True:
             try:
                 self.driver.get(f"{self.base_url}")
-                print(f"{self.base_url}")                
+                print(f"{self.base_url}") # Prints the base url for debugging purposes    
                 break
-            except TimeoutException as timeout:
+            except TimeoutException as timeout: # Catches the timeout exception if the page is unable to load
                 print(f"{timeout}")
                 print(f"Page will be reloaded.")
     
-    # https://stackoverflow.com/questions/29916054/change-user-agent-for-selenium-web-driver
-    
-    
-    def navigate_to_mercados_precios(self):
-        link = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(MainPagePointers.MERCADOS_PRECIOS))
+    def navigate_mercados_precios(self):
+        """
+        Navigates to the *Mercados y precios* link and clicks it.
+        """
+        link = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(MainPagePointers.MERCADOS_PRECIOS))
         self.driver.execute_script("arguments[0].click();", link)
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(30)    
         
         
+    def date_picker(self, year, mnnth, day):
+        """
+        Navigates to each date from a selected range.
+        """
         
-        
+        open_selector = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(MercadosPreciosPointers.SELECT_YEAR))
+        self.driver.execute_script("arguments[0].click();", open_selector)
         
         
         
