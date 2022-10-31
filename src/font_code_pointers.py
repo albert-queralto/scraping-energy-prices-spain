748414de7897d00b09df5b5820fea470d68bd2fc
aqueralto <albert.queralto.lopez@gmail.com>
2022-10-31 18:57:31 +0100

    functional hour_selection

--- src/font_code_pointers.py
+++ src/font_code_pointers.py
@@ -17,36 +17,11 @@ class MercadosPreciosPointers(object):
     HOUR_SELECTOR_HIDDEN_CHILD = (By.XPATH, '//*[@id="mypCosteWidgetView"]/div[1]/div/div/div[2]/div/div/div')
     
     HOUR_LIST_HIDDEN = (By.XPATH, f"//*[@class='chzn-container chzn-container-single']")
-    # HOUR_LIST_HIDDEN_CHILD = (By.XPATH, f"//*[@class='chzn-container chzn-container-single']/a")
+    HOUR_LIST_ACTIVE = (By.XPATH, f"//*[@class='chzn-container chzn-container-single chzn-container-active']")
+    HOUR_LIST_ACTIVE_DROP = (By.XPATH, f"//*[@class='chzn-container chzn-container-single chzn-with-drop chzn-container-active']")
     HOUR_LIST_HIDDEN_CHILD = (By.XPATH, f"//*[@class='chzn-single']")
-    # HOUR_LIST_HIDDEN_CHILD = (By.XPATH, '/html/body/div[3]/div[2]/div/div/div[2]/div/div[1]/div/div/div[2]/div/div/div/div/a/div')
-    
-    
     
     SELECT_HOUR_LIST = (By.XPATH, '//select[contains(@class, "select-timepicker hours")]')    
     FIND_CHZN_DROP = (By.XPATH, f"//*[contains(@class, 'chzn-drop')]")
-    FIND_CHZN_RESULTS = (By.XPATH, '/html/body/div[3]/div[2]/div/div/div[2]/div/div[1]/div/div/div[2]/div/div/div/div/div/ul')
-    
-    
-    
-    
-# class ListopiaPointers(object):
-#     X_BUTTON = (By.CLASS_NAME, 'gr-iconButton')
-#     BEST_BOOKS_EVER = (By.LINK_TEXT, 'Best Books Ever')
-    
-# class BestBooksEverPointers(object):
-#     POSITION_ORDER = ('td', 'number')
-#     BOOK_TITLE = ('a', 'bookTitle')
-#     AUTHOR_NAME = ('a', 'authorName')
-#     STARS_RATINGS = ('span', 'minirating')
-#     SCORE = ('a', 'score:')
-#     VOTES = ('a', 'people voted')
-#     NEXT_PAGE = (By.CSS_SELECTOR, '#all_votes > div.pagination > a.next_page')
-#     NEXT_PAGE_DISABLED = (By.CSS_SELECTOR, '#all_votes > div.pagination > span.next_page.disabled')
-
-
-# class BookPagePointers(object):
-#     BOOK_DESCRIPTION_DIV = ('div', 'BookPageMetadataSection__description')
-#     BOOK_DESCRIPTION_SPAN = ('span', 'Formatted')
-#     PAGES = ('p', 'data-testid', 'pagesFormat')
-#     PUBLICATION_YEAR = ('p', 'data-testid', 'publicationInfo')
+    FIND_CHZN_RESULTS = (By.XPATH, "//*[class='chzn-results']")
+    TEST_FULL_PATH = (By.XPATH, "/html/body/div[3]/div[2]/div/div/div[2]/div/div[1]/div/div/div[2]/div/div/div/div/div/ul")
--- src/navigation.py
+++ src/navigation.py
@@ -1,4 +1,5 @@
 from argparse import Action
+from os import listxattr
 import time
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
@@ -45,8 +46,14 @@ class Navigation(BasePage):
         """
         Navigates to the *Mercados y precios* link and clicks it.
         """
-        link = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(MainPagePointers.MERCADOS_PRECIOS))
-        self.driver.execute_script("arguments[0].click();", link)
+        try:
+            link = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(MainPagePointers.MERCADOS_PRECIOS))
+            # self.driver.execute_script("arguments[0].click();", link)
+            link.click()
+        except TimeoutException as timeout: # Catches the timeout exception if the page is unable to load
+                print(f"{timeout}")
+                print(f"Page will be reloaded.")
+                return self.navigate_mercados_precios()
         
         
     def date_navigator(self, year, month, day):
@@ -62,15 +69,19 @@ class Navigation(BasePage):
         day: int.
                 The day to navigate to.
         """
-        
-        # Check if ?date=DD-MM-YYYY is present in the URL
-        if '?date=' in self.driver.current_url:
-            # If it is present, remove it
-            page_url = self.driver.get(self.driver.current_url.split('?date=')[0])
+        try:
+            # Check if ?date=DD-MM-YYYY is present in the URL
+            if '?date=' in self.driver.current_url:
+                # If it is present, remove it
+                page_url = self.driver.get(self.driver.current_url.split('?date=')[0])
 
-        # Add the '?date=DD-MM-YYYY' string to the URL and go to the page
-        page_url = self.driver.current_url + f"?date={day}-{month}-{year}"
-        return self.driver.get(page_url)
+            # Add the '?date=DD-MM-YYYY' string to the URL and go to the page
+            page_url = self.driver.current_url + f"?date={day}-{month}-{year}"
+            return self.driver.get(page_url)
+        except TimeoutException as timeout: # Catches the timeout exception if the page is unable to load
+                print(f"{timeout}")
+                print(f"Page will be reloaded.")
+                return self.date_navigator(year, month, day)
     
     
     def hour_selection(self, list_index):
@@ -80,63 +91,82 @@ class Navigation(BasePage):
         select_hour_tooltip = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(MercadosPreciosPointers.HOUR_SELECTOR_HIDDEN))
         select_hour_tooltip_child = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(MercadosPreciosPointers.HOUR_SELECTOR_HIDDEN_CHILD))
     
-        print(select_hour_tooltip_child.get_attribute("class"))
+        # print(select_hour_tooltip_child.get_attribute("class"))
         if select_hour_tooltip_child.get_attribute("class") == 'time-selector-tooltip is-hidden':
             select_hour_tooltip.click()
-            print(select_hour_tooltip_child.get_attribute("class"))
+            # print(select_hour_tooltip_child.get_attribute("class"))
+        
+        if list_index > 0 and select_hour_tooltip_child.get_attribute("class") == 'time-selector-tooltip':
+            select_hour_tooltip.click()
+            select_hour_tooltip.click()
 
-
-            select_hour_timepicker = select_hour_tooltip.find_element(MercadosPreciosPointers.HOUR_LIST_HIDDEN[0], MercadosPreciosPointers.HOUR_LIST_HIDDEN[1])
-            select_hour_timepicker_child = select_hour_tooltip.find_element(MercadosPreciosPointers.HOUR_LIST_HIDDEN_CHILD[0], MercadosPreciosPointers.HOUR_LIST_HIDDEN_CHILD[1])
-            
-            print(select_hour_timepicker.get_attribute("class"))
-            if select_hour_timepicker.get_attribute("class") == 'chzn-container chzn-container-single':
-                try:
-                    self.driver.implicitly_wait(5)
-                    select_hour_tooltip_child.click()
-                    time.sleep(5)
-                    print(select_hour_timepicker.get_attribute("class"))
-                    
-                except ElementNotInteractableException as element_not_interactable:
-                    print(element_not_interactable)
-                    return self.hour_selection
+        select_hour_timepicker = select_hour_tooltip.find_element(MercadosPreciosPointers.HOUR_LIST_HIDDEN[0], MercadosPreciosPointers.HOUR_LIST_HIDDEN[1])
+        # print("select_hour_timepicker", select_hour_timepicker.get_attribute("class"))
+        if select_hour_timepicker.get_attribute("class") == 'chzn-container chzn-container-single':
+            try:
+                select_hour_tooltip_child.click()
+                select_drop_down = select_hour_timepicker.find_element(MercadosPreciosPointers.HOUR_LIST_ACTIVE_DROP[0], MercadosPreciosPointers.HOUR_LIST_ACTIVE_DROP[1])
+                time.sleep(1)
                 
-                # click_hour_drop_down = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(MercadosPreciosPointers.HOUR_LISTS_HIDDEN_CHILD))
-                # click_hour_drop_down.click()
-                # move_to_hour_selector = ActionChains(self.driver).move_to_element(select_hour_timepicker_child)
-                # move_to_hour_selector.click(select_hour_timepicker_child).perform()
-            print(select_hour_timepicker.get_attribute("class"))
+            except ElementNotInteractableException as element_not_interactable:
+                print(element_not_interactable)
+                select_hour_tooltip_child.click()
+                time.sleep(1)
+            except NoSuchElementException as no_such_element:
+                print(no_such_element)
+                select_drop_down = select_hour_timepicker.find_element(MercadosPreciosPointers.HOUR_LIST_ACTIVE[0], MercadosPreciosPointers.HOUR_LIST_ACTIVE[1])
+                select_drop_down.click()
 
+        # print("select_hour_timepicker",  select_hour_timepicker.get_attribute("class"))
+        # time.sleep(5)
+            
 
-        # select_hour_list = Select(select_hour_timepicker.find_element(MercadosPreciosPointers.SELECT_HOUR_LIST[0], MercadosPreciosPointers.SELECT_HOUR_LIST[1]))
-        # select_hour_list = select_hour_timepicker.find_element(MercadosPreciosPointers.SELECT_HOUR_LIST[0], MercadosPreciosPointers.SELECT_HOUR_LIST[1])
-        # select_hour_list.click()
-        # test = select_hour_timepicker.find_element(MercadosPreciosPointers.SELECT_HOUR_LIST[0], MercadosPreciosPointers.SELECT_HOUR_LIST[1]).is_displayed()
-        # print(test)
-        # print('Print options', [o.get_attribute('textContent') for o in select_hour_list.options])   
-        # for value in range(len(select_hour_list.options)):
-        #     if value == list_index:
-        #         print(f"visiting element {value}")
-        #         select_hour_list = Select(WebDriverWait(self.driver, 30).until(EC.presence_of_element_located(MercadosPreciosPointers.SELECT_HOUR_LIST)))
-        #         # remove style attribute with "display: none;"
-        #         # self.driver.execute_script("arguments[0].removeAttribute('style');", select_hour_list)
-        #                             #   self.driver.find_element(MercadosPreciosPointers.SELECT_HOUR_LIST[0], MercadosPreciosPointers.SELECT_HOUR_LIST[1]))
-        #         # make value on select() element interactable
-        #         # self.driver.execute_script("arguments[0].removeAttribute('disabled')", select_hour_list.options[value])
-        #         self.driver.execute_script("arguments[0].scrollIntoView();", select_hour_list.options[value])
-        #         # self.driver.execute_script("arguments[0].setAttribute('class', 'active-result highlighted')", select_hour_list.options[value])
-        #         select_hour_list.select_by_value(select_hour_list.options[value].get_attribute("value"))
-        #         time.sleep(1)
-        #         # self.driver.execute_script("arguments[0].setAttribute('class', 'active-result result-selected');", select_hour_list.options[value])
-        #         # self.driver.execute_script("arguments[0].click();", select_hour_list.options[value])
-        #         print(f"element {value} visited")
-        #         time.sleep(5)       
-
-
-
-
-        # find_chzn_results = self.driver.find_element(MercadosPreciosPointers.FIND_CHZN_RESULTS[0], MercadosPreciosPointers.FIND_CHZN_RESULTS[1])
+        # select_hour_timepicker_child = select_hour_tooltip.find_element(MercadosPreciosPointers.HOUR_LIST_HIDDEN_CHILD[0], MercadosPreciosPointers.HOUR_LIST_HIDDEN_CHILD[1])
+        
+        # self.driver.execute_script("arguments[0].click();", select_hour_timepicker_child)
+        # print("select_drop_down", select_drop_down.get_attribute("class"))
+        # time.sleep(5)
+        
+        
+        li_elements = self.driver.find_element(By.XPATH, 
+                                            f'/html/body/div[3]/div[2]/div/div/div[2]/div/div[1]/div/div/div[2]/div/div/div/div/div/ul/li[{list_index+1}]')
+        print(li_elements.get_attribute("textContent"))
+        actions = ActionChains(self.driver)
+        # actions.move_to_element(li_elements).perform()
+        # self.driver.execute_script("arguments[0].scrollIntoView();", li_elements.get(f"{list_index+1}"))
+        actions.move_to_element(WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.XPATH, 
+                                                    f'/html/body/div[3]/div[2]/div/div/div[2]/div/div[1]/div/div/div[2]/div/div/div/div/div/ul')))).perform()
+        if list_index > 6:
+            # actions.send_keys(Keys.ARROW_DOWN).send_keys(Keys.ENTER).perform()
+            self.driver.execute_script("arguments[0].scrollTo(0,0);", li_elements)
+            actions.move_to_element(WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.XPATH, 
+                                                        f'/html/body/div[3]/div[2]/div/div/div[2]/div/div[1]/div/div/div[2]/div/div/div/div/div/ul/li[{list_index+1}]')))).perform()
+        actions.pause(2)
+        actions.click(li_elements).perform()
+        # if list_index > 9 and list_index % 4 == 0:
+        #     actions.send_keys(Keys.ARROW_UP).send_keys(Keys.ENTER).perform()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # li_elements = self.driver.find_element(MercadosPreciosPointers.TEST_FULL_PATH[0], 
+        #                                     f'/html/body/div[3]/div[2]/div/div/div[2]/div/div[1]/div/div/div[2]/div/div/div/div/div/ul/li[{list_index+1}]')
+        
         # find_chzn_drop = self.driver.find_element(MercadosPreciosPointers.FIND_CHZN_DROP[0], MercadosPreciosPointers.FIND_CHZN_DROP[1])
+        # # find_chzn_results = find_chzn_drop.find_element(MercadosPreciosPointers.FIND_CHZN_RESULTS[0], MercadosPreciosPointers.FIND_CHZN_RESULTS[1])
         # find_ul = find_chzn_drop.find_element(By.TAG_NAME, "ul")
         
         # try:
@@ -145,16 +175,26 @@ class Navigation(BasePage):
         #     self.driver.execute_script("arguments[0].scrollIntoView();", find_li_element)
         #     print(find_li_element.get_attribute('textContent'))
         #     if find_li_element.get_attribute("class") != 'active-result result-selected' and list_index > 0:
-        #         # change class of previous element to active-result
-        #         find_li_element_previous = find_ul.find_element(By.XPATH, f"//*[text()={str(list_index - 1).zfill(2)}]")
-        #         print("previous element class:", find_li_element_previous.get_attribute("class"))
-        #         self.driver.execute_script("arguments[0].setAttribute('class', 'active-result');", find_li_element_previous)
-        #         print("previous element class:", find_li_element_previous.get_attribute("class"))
-        #         # set current element class to active-result result-selected
-        #         print("current element class:", find_li_element.get_attribute("class"))
-        #         self.driver.execute_script("arguments[0].setAttribute('class', 'active-result result-selected');", find_li_element)            
-        #         self.driver.execute_script("arguments[0].click();", find_li_element)
-        #         print("current element class:", find_li_element.get_attribute("class"))
+        #         try:
+        #             move_to_hour = ActionChains(self.driver)
+        #             move_to_hour.move_to_element(find_li_element).click().perform()
+        #         except ElementNotInteractableException as element_not_interactable:
+        #             print(element_not_interactable)
+        #             self.driver.execute_script("arguments[0].scrollIntoView();", find_li_element)
+        #             self.driver.execute_script("arguments[0].click();", find_li_element)
+        #             find_li_element.click()
+        #             time.sleep(5)
+        #             # change class of previous element to active-result
+        #             find_li_element_previous = find_ul.find_element(By.XPATH, f"//*[text()={str(list_index - 1).zfill(2)}]")
+        #             print("previous element class:", find_li_element_previous.get_attribute("class"))
+        #             self.driver.execute_script("arguments[0].setAttribute('class', 'active-result');", find_li_element_previous)
+        #             print("previous element class:", find_li_element_previous.get_attribute("class"))
+        #             # set current element class to active-result result-selected
+        #             print("current element class:", find_li_element.get_attribute("class"))
+        #             self.driver.execute_script("arguments[0].setAttribute('class', 'active-result result-selected');", find_li_element)            
+        #             # self.driver.execute_script("arguments[0].click();", find_li_element)
+        #             find_li_element.click()
+        #             print("current element class:", find_li_element.get_attribute("class"))
         #     time.sleep(5)
         # except StaleElementReferenceException as stale_exception:
         #     print(stale_exception)
@@ -162,21 +202,48 @@ class Navigation(BasePage):
         #     self.driver.execute_script("arguments[0].scrollIntoView();", find_li_element)
         #     self.driver.execute_script("arguments[0].click();", find_li_element)
         #     time.sleep(5)
-            
-            
-            
-    
         
         
+        # select_hour_list = Select(select_drop_down.find_element(MercadosPreciosPointers.SELECT_HOUR_LIST[0], MercadosPreciosPointers.SELECT_HOUR_LIST[1]))
+        # select_hour_list = select_hour_timepicker.find_element(MercadosPreciosPointers.SELECT_HOUR_LIST[0], MercadosPreciosPointers.SELECT_HOUR_LIST[1])
 
+        # print('Print options', [o.get_attribute('textContent') for o in select_hour_list.options])   
+        # for value in range(len(select_hour_list.options)):
+        #     if value == list_index:
+        #         print(f"visiting element {value}")
+        #         # select_right_hour = ActionChains(self.driver)
+        #         # select_right_hour.move_to_element(select_hour_list.options[value])
+        #         # select_right_hour.click(select_hour_list.options[value]).perform()
+        #         select_hour_list = Select(WebDriverWait(self.driver, 30).until(EC.presence_of_element_located(MercadosPreciosPointers.SELECT_HOUR_LIST)))
+        #         # select_hour_list.select_by_value(value)
+        #         self.driver.execute_script("arguments[0].style.visibility = 'visible';", select_hour_list.options[value])
+        #         self.driver.execute_script("arguments[0].click();", select_hour_list.options[value])
+        #         # select_hour_list.options[value].click()
 
+        #         # self.driver.execute_script("arguments[0].setAttribute('style', 'display: block;');", select_hour_list.options[value])
+        #         # self.driver.execute_script("arguments[0].scrollIntoView();", select_hour_list.options[value])
+        #         # self.driver.execute_script("arguments[0].click();", select_hour_list.options[value])
+        #         # select_hour_list.select_by_visible_text(f"{str(list_index).zfill(2)}")
+        #         print("Real value of element is:", select_hour_list.first_selected_option.get_attribute('textContent'))
+        #         # remove style attribute with "display: none;"
+        #         # self.driver.execute_script("arguments[0].removeAttribute('style');", select_hour_list)
+        #                             #   self.driver.find_element(MercadosPreciosPointers.SELECT_HOUR_LIST[0], MercadosPreciosPointers.SELECT_HOUR_LIST[1]))
+        #         # make value on select() element interactable
+        #         # self.driver.execute_script("arguments[0].removeAttribute('disabled')", select_hour_list.options[value])
+        #         # self.driver.execute_script("arguments[0].scrollIntoView();", select_hour_list.options[value])
+        #         # self.driver.execute_script("arguments[0].setAttribute('class', 'active-result highlighted')", select_hour_list.options[value])
+        #         # select_hour_list.select_by_value(select_hour_list.options[value].get_attribute("value"))
+        #         # select_hour_list.options[value].click()
+        #         # time.sleep(1)
+        #         # self.driver.execute_script("arguments[0].setAttribute('class', 'active-result result-selected');", select_hour_list.options[value])
+        #         # self.driver.execute_script("arguments[0].click();", select_hour_list.options[value])
+        #         print(f"element {value} visited")
+        #         time.sleep(5)       
         
-
-            # if select_hour_list.get_attribute("class") not in ["active-result result-selected", "active-result highlighted"]:
-                # selectable_hour_element = self.driver.find_element(MercadosPreciosPointers.SELECTABLE_HOUR[0], MercadosPreciosPointers.SELECTABLE_HOUR[1])
-                # move_to_selectable_hour = ActionChains(self.driver).move_to_element(selectable_hour_element)
-                # move_to_selectable_hour.click().perform()
-            # else:
-            # select_hour_list.select_by_value(f'{value}')
-            # print(f"element {value} selected")        
-        
+        # if select_hour_list.get_attribute("class") not in ["active-result result-selected", "active-result highlighted"]:
+            # selectable_hour_element = self.driver.find_element(MercadosPreciosPointers.SELECTABLE_HOUR[0], MercadosPreciosPointers.SELECTABLE_HOUR[1])
+            # move_to_selectable_hour = ActionChains(self.driver).move_to_element(selectable_hour_element)
+            # move_to_selectable_hour.click().perform()
+        # else:
+        # select_hour_list.select_by_value(f'{value}')
+        # print(f"element {value} selected")        
