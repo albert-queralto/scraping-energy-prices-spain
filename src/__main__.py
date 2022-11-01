7c109b110400912fc28d8690ec48f225963163bc
aqueralto <albert.queralto.lopez@gmail.com>
2022-11-01 11:18:18 +0100

    dictionary to store data and added saving method

--- src/__main__.py
+++ src/__main__.py
@@ -38,10 +38,10 @@ class ElectricityScraper(unittest.TestCase):
         self.base_url = 'https://www.esios.ree.es/es' # Base URL that we want to scrape
         
         # Set days before the actual day to scrape and create a list of dates and transform them to tuples
-        self.num_previous_days = 1
+        self.num_previous_days = 2
         self.date_range = [datetime.datetime.today() - datetime.timedelta(days=day) for day in range(self.num_previous_days)]
         self.date_range = [(date.year, date.month, date.day) for date in reversed(self.date_range)]
-        
+                
     def test_scraper(self):
         """
         Function that implements the scraper.
@@ -54,14 +54,48 @@ class ElectricityScraper(unittest.TestCase):
         
         # Navigates through the defined dates
         for year, month, day in self.date_range:
+            
+            # Initialize the variables
+            date = f"{day}-{month}-{year}" # Create the date variable
+            date_data = {'date': [], 'hour': [],
+                        'avg total price (euro/MWh)': [],
+                        'avg price free market (euro/MWh)': [],
+                        'avg price reference market (euro/MWh)': [],
+                        'energy total (MWh)': [],
+                        'energy free market (MWh)': [],
+                        'energy reference market (MWh)': [],
+                        'free market share (%)': [],
+                        'reference market share (%)': []
+            }
+            
             page_navigator.date_navigator(year=year, month=month, day=day)
             time.sleep(5)
-            for i in range(6,24):
-                page_navigator.hour_selection(list_index=i)                
+            print(self.driver.current_url)
+            
+            for i in range(24):
+                page_navigator.hour_selection_mercados_precios(list_index=i)                
                 mercado_precio = MercadoPreciosData(driver=self.driver)
-                print(self.driver.current_url)
                 time.sleep(5)
-                mercado_precio.precio_final_energia()
+                
+                pm_total, pm_com_libre, pm_com_ref = mercado_precio.get_precio_final_energia()
+                energia_total, energia_com_libre, energia_com_ref = mercado_precio.get_energia()
+                cuota_com_libre, cuota_com_ref = mercado_precio.get_cuota()
+                
+                # Append data to dictionary that will be used to save it into a file
+                date_data['date'].append(date)
+                date_data['hour'].append(f"{'0' if i < 10 else ''}{i}:00")
+                date_data['avg total price (euro/MWh)'].append(pm_total)
+                date_data['avg price free market (euro/MWh)'].append(pm_com_libre)
+                date_data['avg price reference market (euro/MWh)'].append(pm_com_ref)
+                date_data['energy total (MWh)'].append(energia_total)
+                date_data['energy free market (MWh)'].append(energia_com_libre)
+                date_data['energy reference market (MWh)'].append(energia_com_ref)
+                date_data['free market share (%)'].append(cuota_com_libre)
+                date_data['reference market share (%)'].append(cuota_com_ref)
+
+            # Save the data to a CSV file for each day
+            data_saver = SaveUtils(filename=f'energy_prices_{date}.csv', dictionary=date_data)
+            data_saver.save_data()
 
     def tearDown(self):
         """
