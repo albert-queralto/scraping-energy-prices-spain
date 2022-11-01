fc0ad9f2589d7b202a18374093dcc678558da97b
aqueralto <albert.queralto.lopez@gmail.com>
2022-11-01 19:20:35 +0100

    added decorator to order unittests and additional comments

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
+# Path: src/__main__.py
+# Authors: Esther Manzano, Albert Queraltó
+
+"""
+This is the main file to execute the bot used to scrape the website: https://www.esios.ree.es/es
+
+Two different areas of the site were scraped: *Mercados y precios* and *Generación y consumo*
+The idea was to obtain daily data from energy prices, as well as data from energy generation 
+from renewable sources that can be later analyzed or used to develop machine learning models.
+
+Unit tests were used during the execution to find and handle any errors and obtain additional
+information of those tests.
+"""
+
 import unittest
 import random
 import time
@@ -5,7 +20,6 @@ import datetime
 import pandas as pd
 
 # Webscraping libraries
-import requests
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -16,13 +30,33 @@ from navigation import *
 from webdriver_utils import *
 from support_functions import *
 
-"""
-https://tarifaluzhora.es/
-https://www.esios.ree.es/es
-"""
+
+def order_unittests():
+    """
+    Allows to control the order in which unittests are performed. Works as a decorator function to be applied on the unittests.
+    Source: https://codereview.stackexchange.com/questions/122532/controlling-the-order-of-unittest-testcases
+    """
+    ordered_tests = {}
+    
+    def ordered_unittests(f):
+        ordered_tests[f.__name__] = len(ordered_tests)
+        return f
+    
+    def compare_order_unittests(a, b):
+        return [1, -1][ordered_tests[a] < ordered_tests[b]]
+
+    return ordered_unittests, compare_order_unittests
+
+# Define the order of unittests
+ordered_unittests, compare_order_unittests = order_unittests()
+unittest.defaultTestLoader.sortTestMethodsUsing = compare_order_unittests
+
 
 class ElectricityScraper(unittest.TestCase):
-    # We will use the unittest library to check if there are any errors during the execution of the Crawler, allowing for better debugging.
+    """
+    Contains the methods used to scrape data from: https://www.esios.ree.es/es
+    The unittest library is used to check for any errors during the execution of the crawler, allowing for better debugging.
+    """
     def setUp(self):
         """
         Method that includes all the elements that the test each test requires for execution.
@@ -43,49 +77,7 @@ class ElectricityScraper(unittest.TestCase):
         self.date_range = [datetime.datetime.today() - datetime.timedelta(days=day) for day in range(self.num_previous_days)]
         self.date_range = [(date.year, date.month, date.day) for date in reversed(self.date_range)]
                 
-                
-    def test_generacion_consumo_scraper(self):
-        """
-        Function that implements the scraper for the *Generación y consumo* page.
-        """
-        
-        # Initiate the navigation elements for the first page and open the Chrome session
-        page_navigator = NavigationMain(driver=self.driver, base_url=self.base_url)
-        page_navigator.open_chrome_session()
-        time.sleep(1)
-        
-        # Go to the *Generación y consumo* page
-        page_navigator.navigate_generacion_consumo()
-        time.sleep(1)
-        
-        # Initiate the method to perform the hour selection in the *Generación y consumo* page
-        generacion_consumo_navigator =  NavigationGeneracionConsumo(driver=self.driver)
-        
-        # Create list with all files in the save directory
-        files = os.listdir('data')
-        
-        # Navigates through the defined date range
-        for year, month, day in self.date_range:
-            date = f"{day}-{month}-{year}" # Create the date variable
-            
-            # Check if day, month and year are in the file name and load the file
-            file = [file for file in files if date in file]
-            energy_prices = pd.read_csv(f"data/{file[0]}", sep=";")
-            
-            # Initiate method to get the data from the *Generación y consumo* page
-            generacion_consumo_navigator.date_navigator(year=year, month=month, day=day)
-            time.sleep(5)
-            print(self.driver.current_url) # For debugging
-            
-            renewable_data_iterator = WrapperFunctionsGeneracionConsumo(date=date, max_hour=2, generacion_consumo_nav=generacion_consumo_navigator, driver=self.driver)
-            renewable_data_df = renewable_data_iterator.hour_iterator_generacion_libre_co2()
-            
-            energy_prices = pd.merge(energy_prices, renewable_data_df, on=['date', 'hour'])
-            
-            # Save the data to a CSV file for each day
-            energy_prices.to_csv(f"data/energy_prices_renewable_generation_{date}.csv", index=False)
-                
-                
+    @ordered_unittests
     def test_mercado_precios_scraper(self):
         """
         Function that implements the scraper for the *Mercado y precios* page.
@@ -119,13 +111,57 @@ class ElectricityScraper(unittest.TestCase):
             data_saver = FileUtils(filename=f'energy_prices_{date}.csv', dictionary=market_price)
             data_saver.save_data()
             
-    
+    @ordered_unittests
+    def test_generacion_consumo_scraper(self):
+        """
+        Function that implements the scraper for the *Generación y consumo* page.
+        """
+        
+        # Initiate the navigation elements for the first page and open the Chrome session
+        page_navigator = NavigationMain(driver=self.driver, base_url=self.base_url)
+        page_navigator.open_chrome_session()
+        time.sleep(1)
+        
+        # Go to the *Generación y consumo* page
+        page_navigator.navigate_generacion_consumo()
+        time.sleep(1)
+        
+        # Initiate the method to perform the hour selection in the *Generación y consumo* page
+        generacion_consumo_navigator =  NavigationGeneracionConsumo(driver=self.driver)
+        
+        # Create list with all files in the save directory
+        files = os.listdir('data')
+        
+        # Navigates through the defined date range
+        for year, month, day in self.date_range:
+            date = f"{day}-{month}-{year}" # Create the date variable
+            
+            # Check if day, month and year are in the file name and load the file
+            file = [file for file in files if date in file]
+            energy_prices = pd.read_csv(f"data/{file[0]}", sep=";")
+            
+            # Initiate method to get the data from the *Generación y consumo* page
+            generacion_consumo_navigator.date_navigator(year=year, month=month, day=day)
+            time.sleep(5)
+            print(self.driver.current_url) # For debugging
+            
+            renewable_data_iterator = WrapperFunctionsGeneracionConsumo(date=date, max_hour=24, generacion_consumo_nav=generacion_consumo_navigator, driver=self.driver)
+            renewable_data_df = renewable_data_iterator.hour_iterator_generacion_libre_co2()
+            
+            energy_prices = pd.merge(energy_prices, renewable_data_df, on=['date', 'hour'])
+            
+            # Save the data to a CSV file for each day
+            energy_prices.to_csv(f"data/energy_prices_renewable_generation_{date}.csv", index=False)
+
 
     def tearDown(self):
         """
         Method that includes all the instructions used after the unittest is performed.
         """
-        self.driver.close() # We close the driver
+        # Close and quit the driver
+        self.driver.close() 
+        self.driver.quit() 
+        
         
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
