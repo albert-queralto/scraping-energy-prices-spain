1f4e98feb515f13b54a64cd4f2cd3f589a6d4b32
aqueralto <albert.queralto.lopez@gmail.com>
2022-11-01 18:42:02 +0100

    added test_generacion_consumo_scraper and wrapped the code in methods

--- src/__main__.py
+++ src/__main__.py
@@ -13,8 +13,8 @@ from selenium.webdriver.chrome.options import Options
 
 # Custom libraries
 from navigation import *
-from data_scraper import *
 from webdriver_utils import *
+from support_functions import *
 
 """
 https://tarifaluzhora.es/
@@ -68,117 +68,56 @@ class ElectricityScraper(unittest.TestCase):
         for year, month, day in self.date_range:
             date = f"{day}-{month}-{year}" # Create the date variable
             
-            # try:
             # Check if day, month and year are in the file name and load the file
             file = [file for file in files if date in file]
-            df = pd.read_csv(f"data/{file[0]}")
-            
-            
-            # Dictionary to store the data for each date before saving it into the loaded file
-            date_data = {'date': [], 'hour': [],
-                        'renewable generation (%)': [],
-                        'renewable generation (MW)': [],
-                        'avg price reference market (euro/MWh)': [],
-                        'wind generation (MW)': [],
-                        'solar generation (MW)': [],
-                        'nuclear generation (MW)': [],
-                        'thermorenewable generation (MW)': []
-            }
+            energy_prices = pd.read_csv(f"data/{file[0]}", sep=";")
             
             # Initiate method to get the data from the *Generación y consumo* page
             generacion_consumo_navigator.date_navigator(year=year, month=month, day=day)
             time.sleep(5)
             print(self.driver.current_url) # For debugging
             
-            # Iterate through the hour elements and select the right hour from the drop down menu list
-            for i in range(24):
-                generacion_consumo_navigator.hour_selection_generacion_libre_co2(list_index=i)                
-                generacion_consumo = GeneracionConsumoData(driver=self.driver)
-                time.sleep(5)
-                
-                # Get the information of prices, energy and shares from *Generación y consumo* page
-                percentage_renewable, renewable_power, wind_power, water_power, solar_power, nuclear_power, thermo_renewable_power = generacion_consumo.get_renewable_generation_data()
-                print(percentage_renewable, renewable_power, wind_power, water_power, solar_power, nuclear_power, thermo_renewable_power)
-                # Append data to dictionary
-                date_data['date'].append(date)
-                date_data['hour'].append(f"{'0' if i < 10 else ''}{i}:00")
-                date_data['avg total price (euro/MWh)'].append(percentage_renewable)
-                date_data['avg price free market (euro/MWh)'].append(renewable_power)
-                date_data['avg price reference market (euro/MWh)'].append(wind_power)
-                date_data['energy total (MWh)'].append(water_power)
-                date_data['energy free market (MWh)'].append(solar_power)
-                date_data['energy reference market (MWh)'].append(nuclear_power)
-                date_data['free market share (%)'].append(thermo_renewable_power)
-                
-            # # Save the data to a CSV file for each day
-            # data_saver = FileUtils(filename=f'energy_prices_{date}.csv', dictionary=date_data)
-            # data_saver.save_data()            
-                
-                
-    # def test_mercado_precios_scraper(self):
-    #     """
-    #     Function that implements the scraper for the *Mercado y precios* page.
-    #     """
-        
-    #     # Initiate the navigation elements for the first page and open the Chrome session
-    #     page_navigator = NavigationMain(driver=self.driver, base_url=self.base_url)
-    #     page_navigator.open_chrome_session()
-    #     time.sleep(1)
-        
-    #     # Go to the *Mercados y precios* page
-    #     page_navigator.navigate_mercados_precios()
-    #     time.sleep(1)
-        
-    #     # Initiate the method to perform the hour selection in the *Mercados y precios* page
-    #     mercado_precio_navigator =  NavigationMercadosPrecios(driver=self.driver)
-        
-    #     # Navigates through the defined date range
-    #     for year, month, day in self.date_range:
-    #         date = f"{day}-{month}-{year}" # Create the date variable
+            renewable_data_iterator = WrapperFunctionsGeneracionConsumo(date=date, max_hour=2, generacion_consumo_nav=generacion_consumo_navigator, driver=self.driver)
+            renewable_data_df = renewable_data_iterator.hour_iterator_generacion_libre_co2()
             
-    #         # Dictionary to store the data for each date before saving it into a file
-    #         date_data = {'date': [], 'hour': [],
-    #                     'avg total price (euro/MWh)': [],
-    #                     'avg price free market (euro/MWh)': [],
-    #                     'avg price reference market (euro/MWh)': [],
-    #                     'energy total (MWh)': [],
-    #                     'energy free market (MWh)': [],
-    #                     'energy reference market (MWh)': [],
-    #                     'free market share (%)': [],
-    #                     'reference market share (%)': []
-    #         }
+            energy_prices = pd.merge(energy_prices, renewable_data_df, on=['date', 'hour'])
             
-    #         # Initiate method to get the data from the *Mercados y precios* page
-    #         mercado_precio_navigator.date_navigator(year=year, month=month, day=day)
-    #         time.sleep(5)
-    #         print(self.driver.current_url) # For debugging
+            # Save the data to a CSV file for each day
+            energy_prices.to_csv(f"data/energy_prices_renewable_generation_{date}.csv", index=False)
+                
+                
+    def test_mercado_precios_scraper(self):
+        """
+        Function that implements the scraper for the *Mercado y precios* page.
+        """
+        
+        # Initiate the navigation elements for the first page and open the Chrome session
+        page_navigator = NavigationMain(driver=self.driver, base_url=self.base_url)
+        page_navigator.open_chrome_session()
+        time.sleep(1)
+        
+        # Go to the *Mercados y precios* page
+        page_navigator.navigate_mercados_precios()
+        time.sleep(1)
+        
+        # Initiate the method to perform the hour selection in the *Mercados y precios* page
+        mercado_precio_navigator =  NavigationMercadosPrecios(driver=self.driver)
+        
+        # Navigates through the defined date range
+        for year, month, day in self.date_range:
+            date = f"{day}-{month}-{year}" # Create the date variable
             
-    #         # Iterate through the hour elements and select the right hour from the drop down menu list
-    #         for i in range(24):
-    #             mercado_precio_navigator.hour_selection_mercados_precios(list_index=i)                
-    #             mercado_precio = MercadoPreciosData(driver=self.driver)
-    #             time.sleep(5)
+            # Initiate method to get the data from the *Mercados y precios* page
+            mercado_precio_navigator.date_navigator(year=year, month=month, day=day)
+            time.sleep(5)
+            print(self.driver.current_url) # For debugging
+            
+            market_prices_data_iterator = WrapperFunctionsMercadoPrecios(date=date, max_hour=24, mercado_precio_nav=mercado_precio_navigator, driver=self.driver)
+            market_price = market_prices_data_iterator.hour_iterator_mercado_precios()
                 
-    #             # Get the information of prices, energy and shares from *Mercado y precios* page
-    #             pm_total, pm_com_libre, pm_com_ref = mercado_precio.get_precio_final_energia()
-    #             energia_total, energia_com_libre, energia_com_ref = mercado_precio.get_energia()
-    #             cuota_com_libre, cuota_com_ref = mercado_precio.get_cuota()
-                
-    #             # Append data to dictionary
-    #             date_data['date'].append(date)
-    #             date_data['hour'].append(f"{'0' if i < 10 else ''}{i}:00")
-    #             date_data['avg total price (euro/MWh)'].append(pm_total)
-    #             date_data['avg price free market (euro/MWh)'].append(pm_com_libre)
-    #             date_data['avg price reference market (euro/MWh)'].append(pm_com_ref)
-    #             date_data['energy total (MWh)'].append(energia_total)
-    #             date_data['energy free market (MWh)'].append(energia_com_libre)
-    #             date_data['energy reference market (MWh)'].append(energia_com_ref)
-    #             date_data['free market share (%)'].append(cuota_com_libre)
-    #             date_data['reference market share (%)'].append(cuota_com_ref)
-                
-    #         # Save the data to a CSV file for each day
-    #         data_saver = FileUtils(filename=f'energy_prices_{date}.csv', dictionary=date_data)
-    #         data_saver.save_data()
+            # Save the data to a CSV file for each day
+            data_saver = FileUtils(filename=f'energy_prices_{date}.csv', dictionary=market_price)
+            data_saver.save_data()
             
     
 
